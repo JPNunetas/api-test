e1f9494a0f5e5366b1ed67583ede7c08333e459d
JPNunetas <jppassaria017@gmail.com>
2025-09-29 20:23:39 +0100

    Setup API!

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+import express, { Request, Response } from "express";
+import 'dotenv/config';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/hello", (_req: Request, res: Response) => {
+    res.status(200).send({
+        status: 200,
+        message: "Hello World!"
+    });
+});
+
+app.listen(port, () => {
+    console.log(`App is online at: http://localhost:${port}`);
+})
